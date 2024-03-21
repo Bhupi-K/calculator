5d13823bc0f74ef4681ceb137dce25f5dfb41f72
Bhupi-K <kumarbhupinder0201@gmail.com>
2024-03-21 10:45:49 -0700

    Division issue in java solved

--- public/js/calc.js
+++ public/js/calc.js
@@ -49,7 +49,7 @@ for (var i = 0; i < operator.length; i++) {
       lastChar === "+" ||
       lastChar === "-" ||
       lastChar === "x" ||
-      lastChar === "÷" 
+      lastChar === "/" 
     ) {
       var newString =
         currentString.substring(0, currentString.length - 1) +
